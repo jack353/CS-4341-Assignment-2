99b7e1051318609c572c8bd283e7776ad3364579
Jack353 <jvc353123@gmail.com>
2022-02-09 14:34:06 -0500

    edits to puzzle 1

--- src/BinGene.java
+++ src/BinGene.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class BinGene {
 
+    ArrayList<Float> bin = new ArrayList<>();
 
-
+    public void add(float n){
+        bin.add(n);
+    }
 }
--- src/BinIndividual.java
+++ src/BinIndividual.java
@@ -1,13 +1,32 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class BinIndividual {
 
     int fitness = 0;
-    BinGene[] genes;
+
+    ArrayList<BinGene> bins = new ArrayList<>();
 
     public BinIndividual(){
-
+        bins.add(new BinGene());
+        bins.add(new BinGene());
+        bins.add(new BinGene());
+        bins.add(new BinGene());
+        fillBins(40);
         findFitness();
+    }
+
+    private ArrayList<Integer> fillBins(int size){
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        Random randomGenerator = new Random();
+        while (numbers.size() < size) {
+            int random = randomGenerator.nextInt(size);
+            if (!numbers.contains(random)) {
+                numbers.add(random);
+                bins.get(Math.floor(numbers.size()/10).add(.get(random));
+            }
+        }
+        return numbers;
     }
 
     public void findFitness(){
