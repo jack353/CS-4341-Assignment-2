534245aea639f8d2b058851e1b8759531a6d32e9
Jack353 <jvc353123@gmail.com>
2022-02-11 17:40:22 -0500

    puzzle 2 logic done, untested

--- src/TowerIndividual.java
+++ src/TowerIndividual.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class TowerIndividual {
 
@@ -6,14 +7,73 @@ public class TowerIndividual {
     int totalCost = 0;
     double cumProb = 0;
 
-
+    ArrayList<TowerPiece> tower = new ArrayList<>();
 
     public TowerIndividual(){
-        //bins.add(new BinGene());
-
-        //fillBins(40);
+        randomFill();
     }
 
+    void randomFill(){
+        Random rg = new Random();
+        int height = rg.nextInt(towerInput.size()) + 1;
+        ArrayList<Integer> used = new ArrayList<>();
+        while(height != 0){
+            int random = rg.nextInt(towerInput.size());
+            if (!used.contains(random)) {
+                tower.add(towerInput.get(random));
+                height--;
+            }
+        }
+    }
+
+    void calcFitness(){
+        getTotalCost();
+        if (tower.size() > 1)
+            checkTopAndBottom();
+    }
+
+    void getTotalCost(){
+        for (TowerPiece p : tower)
+            totalCost += p.cost;
+    }
+
+    void checkTopAndBottom(){
+        if (tower.get(0).type.equals("Door") && tower.get(tower.size()-1).type.equals("Lookout")){
+            if(tower.size() > 2)
+                checkWalls();
+            else
+                checkWAndS();
+        }
+    }
+
+    void checkWalls(){
+        boolean invalid = false;
+        for (int i = 1; i < tower.size()-1; i++){
+            if (!tower.get(i).type.equals("Wall")) {
+                invalid = true;
+                break;
+            }
+        }
+        if (!invalid)
+            checkWAndS();
+    }
+
+    void checkWAndS(){
+        int prevW = tower.get(0).width;
+        boolean valid = true;
+        for (int i = 1; i < tower.size(); i++){
+            if (tower.get(i).width > prevW || tower.get(i).strength < (tower.size() - i-1)){
+                valid = false;
+                break;
+            }
+        }
+        if (valid)
+            getScore();
+    }
+
+    void getScore(){
+        fitness = 10 + (tower.size()^2) -  totalCost;
+    }
 
     void setCumProb(float n){
         cumProb = n;
--- src/TowerPopulation.java
+++ src/TowerPopulation.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class TowerPopulation {
     int size;
@@ -16,8 +17,8 @@ public class TowerPopulation {
     }
 
     public void findFitnesses(){
-        for (int i = 0; i < individuals.size(); i++);
-//            individuals.get(i).findFitness();
+        for (int i = 0; i < individuals.size(); i++)
+            individuals.get(i).calcFitness();
     }
 
     public  void cull(){
@@ -61,5 +62,124 @@ public class TowerPopulation {
             sum += i.fitness;
         }
         return sum;
+    }
+
+    void haveKids(){
+        addFittestTwo();
+
+        while(children.size() < size){
+            addTwoChildren();
+        }
+        individuals = children;
+        children = new ArrayList<>();
+    }
+
+    void addFittestTwo() {
+        children.add(individuals.get(individuals.size()-1));
+        children.add(individuals.get(individuals.size()-2));
+    }
+
+    void addTwoChildren(){
+        TowerIndividual p1 = getParent();
+        TowerIndividual p2 = getParent();
+
+        TowerIndividual c1 = new TowerIndividual();
+        TowerIndividual c2 = new TowerIndividual();
+
+        children.add(c1);
+        children.add(c2);
+
+        crossOver(p1, p2);
+
+        mutate(children.get(children.size()-2), children.get(children.size()-1));
+    }
+
+    TowerIndividual getParent(){
+        double p = Math.random();
+
+        for (TowerIndividual i : individuals){
+            if (i.cumProb > p) {
+                return (i);
+            }
+        }
+        return(null);
+    }
+
+    void crossOver(TowerIndividual p1, TowerIndividual p2){
+        Random rg = new Random();
+        int p1M = rg.nextInt(p1.tower.size());
+        int p2M = rg.nextInt(p2.tower.size());
+
+        ArrayList<TowerPiece> c1 = new ArrayList<>();
+        c1.addAll(p1.tower.subList(0, p1M));
+        c1.addAll(p2.tower.subList(p2M, p2.tower.size()));
+
+        children.get(children.size()-2).tower = c1;
+
+        ArrayList<TowerPiece> c2 = new ArrayList<>();
+        c2.addAll(p2.tower.subList(0, p2M));
+        c2.addAll(p1.tower.subList(p1M, p1.tower.size()));
+
+        children.get(children.size()-1).tower = c2;
+    }
+
+    void mutate(TowerIndividual c1, TowerIndividual c2){
+        if (c1.tower.size() > towerInput.size() || c2.tower.size() > towerInput.size())
+            mutateDel(c1, c2);
+        else
+            mutateSwap(c1, c2);
+    }
+
+    void mutateDel(TowerIndividual c1, TowerIndividual c2){
+        ArrayList<TowerPiece> c1Found = new ArrayList<>();
+        ArrayList<TowerPiece> c2Found = new ArrayList<>();
+
+        for (int i = 0; i < c1.tower.size(); i++){
+            if (c1Found.contains(c1.tower.get(i)))
+                children.get(children.size()-2).tower.remove(i);
+            else
+                c1Found.add(c1.tower.get(i));
+        }
+
+        for (int i = 0; i < c2.tower.size(); i++){
+            if (c2Found.contains(c2.tower.get(i)))
+                children.get(children.size()-1).tower.remove(i);
+            else
+                c1Found.add(c2.tower.get(i));
+        }
+    }
+
+    void mutateSwap(TowerIndividual c1, TowerIndividual c2){
+        ArrayList<TowerPiece> c1Dupes = getDuplicate(c1);
+        ArrayList<TowerPiece> c2Dupes = getDuplicate(c2);
+
+        ArrayList<TowerPiece> c1Found = new ArrayList<>();
+        ArrayList<TowerPiece> c2Found = new ArrayList<>();
+
+        for (int i = 0; i < c1.tower.size(); i++){
+            if (c1Found.contains(c1.tower.get(i)))
+                children.get(children.size()-2).tower.set(i,c2Dupes.remove(i));
+            else
+                c1Found.add(c1.tower.get(i));
+        }
+
+        for (int i = 0; i < c2.tower.size(); i++){
+            if (c2Found.contains(c2.tower.get(i)))
+                children.get(children.size()-1).tower.set(i,c1Dupes.remove(i));
+            else
+                c1Found.add(c2.tower.get(i));
+        }
+    }
+
+    ArrayList<TowerPiece> getDuplicate(TowerIndividual p){
+        ArrayList<TowerPiece> found = new ArrayList<>();
+        ArrayList<TowerPiece> dups = new ArrayList<>();
+        for (TowerPiece pc : p.tower){
+                if (found.contains(pc))
+                    dups.add(pc);
+                else
+                    found.add(pc);
+        }
+        return(dups);
     }
 }
